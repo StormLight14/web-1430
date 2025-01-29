a13ec79186213334c74c4c47d41330299baa272d
stormyyy <stormyyy@noreply.codeberg.org>
2025-01-28 17:38:42 -0700

    add functional chat UI

--- stormchat/script.js
+++ stormchat/script.js
@@ -1,17 +1,45 @@
-let socket = new WebSocket("wss://chat.stormyyy.dev");
-let connected = false;
+async function connect() {
+  return new Promise((resolve, reject) => {
+    const socket = new WebSocket("wss://chat.stormyyy.dev");
 
-socket.onopen = function(event) {
-  console.log("Connection with server (chat.stormyyy.dev) established!");
-  connected = true;
-};
+    socket.onopen = function(event) {
+      console.log("Connection with server established!");
+      resolve(socket);
+    };
 
-socket.onclose = function(event) {
-  console.log("Connection with server closed.");
-  connected = false;
-};
+    socket.onerror = function (error) {
+      reject(error);
+    };
+  })
+}
 
-let username = prompt("Enter your username: ", "storm");
-let password = prompt("Enter your password", "abc123!");
-let msg_content = prompt("Enter a message to send: ");
-socket.send(`${username},${password},${msg_content}`);
+(async () => {
+  try {
+    const socket = await connect();
+    console.log("WebSocket connected:", socket);
+    
+    let username = prompt("Enter your username: ", "storm");
+    let password = prompt("Enter your password", "abc123!");
+
+    document.forms.message_send.onsubmit = function() {
+      let out_msg = this.message.value;
+      socket.send(`${username},${password},${out_msg}`);
+      this.reset();
+      this.message.focus();
+      return false;
+    }
+
+    socket.onmessage = function(event) {
+      let msg = event.data;
+      let msgElement = document.createElement("div");
+      msgElement.textContent = msg;
+      document.getElementById("messages").prepend(msgElement);
+    }
+  } catch (error) {
+    console.error("Failed to connect:", error);
+  }
+})();
+
+
+
+
