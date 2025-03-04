fe40a7957e8863212a002346eb52b4caf7dd587f
stormyyy <stormyyy@noreply.codeberg.org>
2025-03-04 09:47:02 -0700

    finish lesson8 countdown project

--- lesson8/script.js
+++ lesson8/script.js
@@ -1,6 +1,6 @@
 let interval_id;
 
-function initializeCountdown() {
+function initialize_countdown() {
     const main_container = document.getElementById("main-container");
 
     let h1 = document.createElement("h1");
@@ -13,17 +13,17 @@ function initializeCountdown() {
     let button_one = document.createElement("button");
     button_one.innerText = "Start Countdown";
     button_one.id = "start-countdown";
-    button_one.onclick = () => startCountdown();
+    button_one.onclick = () => start_countdown();
 
     let button_two = document.createElement("button");
     button_two.innerText = "Stop Countdown";
     button_two.id = "stop-countdown";
-    button_two.onclick = () => stopCountdown();
+    button_two.onclick = () => stop_countdown();
 
     let button_three = document.createElement("button");
     button_three.innerText = "Set Countdown to Zero";
     button_three.id = "zero-countdown";
-    button_three.onclick = () => zeroCountdown();
+    button_three.onclick = () => zero_countdown();
 
     main_container.appendChild(h1);
     main_container.appendChild(countdown_display);
@@ -32,7 +32,7 @@ function initializeCountdown() {
     main_container.appendChild(button_three);
 }
 
-function startCountdown() {
+function start_countdown() {
     const grad_date = new Date(2025, 7, 1);
     const countdown_display = document.getElementById("countdown-display");
     if (interval_id) {
@@ -42,7 +42,7 @@ function startCountdown() {
         const to_grad_date = grad_date - new Date();
 
         if (to_grad_date <= 0) {
-            zeroCountdown();
+            zero_countdown();
         }
 
         // is there an easier way to do this??
@@ -55,19 +55,73 @@ function startCountdown() {
     }, 1000);
 }
 
-function stopCountdown() {
+function stop_countdown() {
     if (interval_id) {
         clearInterval(interval_id);
     }
 }
 
 // sets countdown to zero
-function zeroCountdown() {
+function zero_countdown() {
     const countdown_display = document.getElementById("countdown-display");
     if (interval_id) {
         clearInterval(interval_id);
     }
-    countdown_display.innerText = "Congratulations!";
+
+    //fetch_celebration_message_callback((message) => {
+        //countdown_display.innerText = message;
+    //});
+
+    fetch_celebration_message_promise()
+        .then((message) => {
+            countdown_display.innerText = message;
+        }).catch((error) => {
+            console.error(error);
+        })
 }
 
-initializeCountdown();
+function fetch_celebration_message_callback(callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "celebrations.json", true);
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const celebrations = JSON.parse(xhr.responseText);
+            const random_message = celebrations["messages"][Math.floor(Math.random() * celebrations["messages"].length)];
+            callback(random_message);
+        } else {
+            console.error("Couldn't get celebrations.json file");
+        }
+    };
+    
+    xhr.onerror = function() {
+        console.error("Error while making the xhr request");
+    };
+    
+    xhr.send();
+}
+
+function fetch_celebration_message_promise() {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "celebrations.json", true);
+        
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                const celebrations = JSON.parse(xhr.responseText);
+                const random_message = celebrations["messages"][Math.floor(Math.random() * celebrations["messages"].length)];
+                resolve(random_message);
+            } else {
+                reject("Couldn't get the celebrations.json file");
+            }
+        };
+        
+        xhr.onerror = function() {
+            reject("Error while making the xhr request");
+        };
+        
+        xhr.send();
+    });
+}
+
+initialize_countdown();
