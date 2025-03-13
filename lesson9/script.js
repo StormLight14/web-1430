c6eddc69583f2f6ba9983d54cdaff982530d03eb
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2025-03-13 21:52:16 +0000

    add basic weather fetch

---
+++ lesson9/script.js
@@ -0,0 +1,38 @@
+const main_container = document.getElementById("main-container");
+let weather_button = null;
+let city_input = null;
+let current_city = "Ghent";
+
+function initalize_weather_app() {
+  let h1 = document.createElement("h1");
+  h1.innerText = "Wacky Weather App";
+  main_container.appendChild(h1);
+
+  city_input = document.createElement("input");
+  city_input.value = current_city;
+  city_input.placeholder = "Enter a city name...";
+  city_input.id = "city-input";
+  main_container.appendChild(city_input);
+
+  weather_button = document.createElement("button");
+  weather_button.innerText = "Get Weather!";
+  weather_button.id = "weather-button";
+  main_container.appendChild(weather_button);
+}
+
+async function fetch_weather_data() {
+  let responseData = fetch(`https://api.weatherapi.com/v1/current.json?key=56a5732619044108945214503251303&q=${current_city}`, {
+    method: "GET"
+  }).await;
+}
+
+initalize_weather_app();
+if (weather_button) {
+  weather_button.onclick = fetch_weather_data;
+}
+
+if (city_input) {
+  city_input.addEventListener("change", (_event) => {
+    current_city = city_input.value;
+  });
+}
