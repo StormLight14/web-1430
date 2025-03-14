2069bc00c003a5ae7888ae3955ac76bdff139d53
stormyyy <stormyyy@noreply.codeberg.org>
2025-03-13 18:55:18 -0600

    save last searched city in localstorage

--- lesson9/script.js
+++ lesson9/script.js
@@ -8,9 +8,14 @@ const condition_label = document.getElementById("condition");
 const humidity_label = document.getElementById("humidity");
 const temperature_label = document.getElementById("temperature");
 const feelslike_label = document.getElementById("feelslike")
+const windspeed_label = document.getElementById("windspeed");
 const icon = document.getElementById("icon");
 
 function initalize_weather_app() {
+  if (localStorage.getItem("last_city")) {
+    current_city = localStorage.getItem("last_city");
+  }
+  
   weather_button = document.createElement("button");
   weather_button.innerText = "Get Weather!";
   weather_button.id = "weather-button";
@@ -42,32 +47,44 @@ async function fetch_weather_data() {
     update_weather_labels(response_data);
   } catch(err) {
     console.error(err);
+    update_weather_labels(null);
   }
 }
 
 function update_weather_labels(weather_data) {
-  current_city_label.innerText = `${current_city}'s Weather`
-  country_label.innerText = `Country: ${weather_data.location.country}`;
-  condition_label.innerText = `Current Condition: ${weather_data.current.condition.text}`;
-  humidity_label.innerText = `Humidity: ${weather_data.current.humidity}%`
-  temperature_label.innerText = `Temperature: ${weather_data.current.temp_c} °C (${weather_data.current.temp_f} °F)`;
-  feelslike_label.innerText = `Feels Like: ${weather_data.current.feelslike_c} °C (${weather_data.current.feelslike_f} °F)`;
-  icon.setAttribute("src", weather_data.current.condition.icon);
+  if (weather_data) {
+    localStorage.setItem("last_city", current_city);
+    current_city_label.innerText = `${current_city}'s Weather`
+    country_label.innerText = `Country: ${weather_data.location.country}`;
+    condition_label.innerText = `Current Condition: ${weather_data.current.condition.text}`;
+    humidity_label.innerText = `Humidity: ${weather_data.current.humidity}%`
+    temperature_label.innerText = `Temperature: ${weather_data.current.temp_c} °C (${weather_data.current.temp_f} °F)`;
+    feelslike_label.innerText = `Feels Like: ${weather_data.current.feelslike_c} °C (${weather_data.current.feelslike_f} °F)`;
+    windspeed_label.innerText = `Wind Speed: ${weather_data.current.wind_kph} km/h (${weather_data.current.wind_mph} mph)`;
+    icon.setAttribute("src", weather_data.current.condition.icon);
+  } else {
+    current_city_label.innerText = `Could not retrieve weather data for ${current_city}`;
+    country_label.innerText = ``;
+    condition_label.innerText = ``;
+    humidity_label.innerText = ``;
+    temperature_label.innerText = ``;
+    feelslike_label.innerText = ``;
+    windspeed_label.innerText = ``;
+    icon.setAttribute("src", "");
+  }
 }
 
 initalize_weather_app();
-if (weather_button) {
-  weather_button.onclick = fetch_weather_data;
-}
-
 document.addEventListener("keydown", (event) => {
   if (event.key == "Enter") {
+    current_city = city_input.value;
     fetch_weather_data();
   }
 });
 
-if (city_input) {
-  city_input.addEventListener("change", (_event) => {
+if (weather_button) {
+  weather_button.onclick = () => {
     current_city = city_input.value;
-  });
+    fetch_weather_data();
+  }
 }
