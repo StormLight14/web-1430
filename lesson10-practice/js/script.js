f645b4b6b7f51f05f646bbc2ea6783c6231289c1
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2025-03-24 10:34:31 -0600

    display popular movies

--- lesson10-practice/js/script.js
+++ lesson10-practice/js/script.js
@@ -7,6 +7,7 @@ function init() {
     case '/':
     case '/index.html':
       console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
@@ -30,12 +31,42 @@ document.addEventListener('DOMContentLoaded', init);
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
   links.forEach(link => {
-    if (link.getAttribute('href' === global.currentPage)) {
+    if (link.getAttribute('href') === global.currentPage) {
       link.classList.add('active');
-    } else {
-      console.log("page doesnt match");
-      console.log(link.getAttribute('href'));
-      console.log(global.currentPage);
     }
+  });
+}
+
+async function fetchAPIData(endpoint) {
+  const API_KEY = '4e04d16f097b644ff6dca9863d09360e';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+  const data = await response.json();
+  
+  return data;
+}
+
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        ${movie.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}"</img>`
+          : `<img src="../images/no-image.jpg class="card-img-top" alt="${movie.title}>`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release Date: ${movie.release_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector('#popular-movies').appendChild(div);
   });
 }
