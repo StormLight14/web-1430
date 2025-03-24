4c61ffe6a7d6424292b7a33aebeca14d819426e2
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2025-03-24 11:07:43 -0600

    display movie details

--- lesson10-practice/js/script.js
+++ lesson10-practice/js/script.js
@@ -12,7 +12,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
@@ -98,6 +98,65 @@ async function displayPopularShows() {
 
     document.querySelector('#popular-shows').appendChild(div);
   });
+}
+
+function addCommasToNumber(number) {
+  let numStr = number.toString();
+
+  let parts = numStr.split('.');
+
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  return parts.join('.');
+}
+
+
+
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.getElementById('movie-details');
+  console.log(div);
+  div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${movie.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top alt="${movie.title}"` : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`}
+      
+    </div>
+    <div>
+      <h2>Movie Title</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map((genre) => `<li>${genre.name}<li>`).join('')}
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+      <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${movie.production_companies.map(company => `<span>${company.name}</span>`, ', ')}</div>
+  </div>
+  `;
 }
 
 function showSpinner() {
