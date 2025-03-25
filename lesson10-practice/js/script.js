ac2f01f1bebe88ceaa588fd8c45ec3ae6214aca5
stormyyy <stormyyy@noreply.codeberg.org>
2025-03-24 20:26:15 -0600

    display show details

--- lesson10-practice/js/script.js
+++ lesson10-practice/js/script.js
@@ -15,7 +15,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('TV Details');
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
@@ -129,7 +129,7 @@ async function displayMovieDetails() {
       
     </div>
     <div>
-      <h2>Movie Title</h2>
+      <h2>${movie.title}</h2>
       <p>
         <i class="fas fa-star text-primary"></i>
         ${movie.vote_average.toFixed(1)} / 10
@@ -161,6 +161,54 @@ async function displayMovieDetails() {
   displayBackgroundImage('movie', movie.backdrop_path);
 }
 
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  const div = document.getElementById('show-details');
+
+  div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top alt="${show.name}"` : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.name}"
+    />`}
+      
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}<li>`).join('')}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Last Episode to Air:</span> ${show.last_episode_to_air.name}}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${show.production_companies.map(company => `<span>${company.name}</span>`, ', ')}</div>
+  </div>
+  `;
+
+  displayBackgroundImage('show', show.backdrop_path);
+}
+
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement('div');
   overlayDiv.style.backgroundImage = `url("https://image.tmdb.org/t/p/original${backgroundPath}")`;
@@ -180,7 +228,7 @@ function displayBackgroundImage(type, backgroundPath) {
     document.querySelector('#movie-details').appendChild(overlayDiv);
   } else {
     console.log(overlayDiv.style.backgroundImage);
-    document.querySelector('#tv-details').appendChild(overlayDiv);
+    document.querySelector('#show-details').appendChild(overlayDiv);
   }
 }
 
