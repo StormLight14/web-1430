79b2ffe57719d3653637164d9f3413e56b3aee3b
stormyyy <stormyyy@noreply.codeberg.org>
2025-03-24 21:29:31 -0600

    add now playing swiper

--- lesson10-practice/js/script.js
+++ lesson10-practice/js/script.js
@@ -6,6 +6,7 @@ function init() {
   switch(global.currentPage) {
     case '/':
     case '/index.html':
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
@@ -207,6 +208,53 @@ async function displayShowDetails() {
   `;
 
   displayBackgroundImage('show', show.backdrop_path);
+}
+
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+
+  results.forEach(movie => {
+    console.log(movie)
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
+      </h4>
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+  });
+  initSwiper();
+}
+
+function initSwiper() {
+  console.log('init swiper')
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      750: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  });
 }
 
 function displayBackgroundImage(type, backgroundPath) {
