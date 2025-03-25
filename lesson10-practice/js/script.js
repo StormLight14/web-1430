8401985554775727095544f1801caf235d0e88f8
stormyyy <stormyyy@noreply.codeberg.org>
2025-03-24 22:27:41 -0600

    add searching

--- lesson10-practice/js/script.js
+++ lesson10-practice/js/script.js
@@ -295,11 +295,44 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const result = await searchAPIData();
-    console.log(result);
+    const { results, total_pages, page } = await searchAPIData();
+    if (results.length === 0) {
+      showAlert('No results found.');
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector('#search-term').value = '';
   } else {
     showAlert('Please enter a search term!');
   }
+}
+
+function displaySearchResults(results) {
+  results.forEach(result => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="${global.search.type}-details.html?id=${result.id}">
+        ${
+          result.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500/${result.poster_path}" class="card-img-top" alt="${global.search.type === 'movie' ? result.title : result.name}"`
+          : `<img src="images/no-image.jpg" class="card-img-top" alt="${global.search.type === 'movie' ? result.title : result.name}"`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: 
+            ${global.search.type === 'movie' ? result.release_date : result.first_air_date}
+          </small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector('#search-results').appendChild(div);
+  });
 }
 
 async function searchAPIData() {
@@ -309,7 +342,7 @@ async function searchAPIData() {
     `${global.api.url}search/${global.search.type}?api_key=${global.api.key}&language=en-US&query=${global.search.term}`
   )
 
-  const data = await response.data;
+  const data = await response.json();
 
   hideSpinner();
 
